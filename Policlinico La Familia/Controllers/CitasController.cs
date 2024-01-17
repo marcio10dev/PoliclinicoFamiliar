12e8eb4c95b205cee44ff7704b8ece172be221c6
david esthyf renjifo flores <tj.david.renjifo.f@upds.net.bo>
2024-01-17 19:31:49 -0400

    CRUD CITAS

---
+++ Policlinico La Familia/Controllers/CitasController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Policlinico_La_Familia.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CitasController : ControllerBase
+    {
+        // GET: api/<CitasController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<CitasController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CitasController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CitasController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CitasController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
